902d74ab759485d8cbd5630047fe1e4a535af053
Magloire AMINOU <aminousem@gmail.com>
2017-04-18 12:01:56 +0000

    add bd credentials

---
+++ public_html/includes/env_properties/live.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * ======================================================
+ * Database Setup
+ * ======================================================
+ */
+$DB_HOST  	= "localhost";
+$DB_USER  	= "wablo";
+$DB_PASSWORD    = "Wablo123#@!";
+$DATABASE  	= "inov_wablo";
+$index_page="index.php";
+$BASE_URL="http://wabloculture.com";
+
+
+?>
